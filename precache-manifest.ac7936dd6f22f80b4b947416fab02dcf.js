a55f87e77a2b3870e66a66745d304b58ec809df5
sdanieau8 <psdanieau@gmail.com>
2022-03-21 11:26:09 -0700

    Updates

---
+++ precache-manifest.ac7936dd6f22f80b4b947416fab02dcf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e1413044a50f72670f1",
+    "url": "/mathreact/static/css/main.a43eb289.chunk.css"
+  },
+  {
+    "revision": "7e1413044a50f72670f1",
+    "url": "/mathreact/static/js/main.7e141304.chunk.js"
+  },
+  {
+    "revision": "4f1dff4311f08c107f6e",
+    "url": "/mathreact/static/js/1.4f1dff43.chunk.js"
+  },
+  {
+    "revision": "d189e4d215ac59767a58",
+    "url": "/mathreact/static/js/runtime~main.d189e4d2.js"
+  },
+  {
+    "revision": "513476e49fcfddb882d4b8d28b029310",
+    "url": "/mathreact/index.html"
+  }
+];
